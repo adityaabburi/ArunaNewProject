c8406de85a8a0db53b495f8578eefe4beb589b59
Aruna Abburi <aruna.abburi@yahoo.com>
2024-11-10 21:33:49 +0100

    new class added

---
+++ src/main/java/org/example/NewTestExample.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class NewTestExample {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,5 +1,13 @@
+import org.example.NewTestExample;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NewTest {
 
     @Test
@@ -11,7 +19,19 @@ public class NewTest {
         Assert.assertTrue(true);
     }
     @Test
-    public void addvalue3(){
-        Assert.assertTrue(false);
+    @DisplayName("addition method success")
+    public void addtwonumbes() {
+        NewTestExample cal = new NewTestExample();
+        int value = cal.add(5, 5);
+        assertEquals(10, value);
     }
+
+
+
+
+
+//    @Test
+//    public void addvalue3(){
+//        Assert.assertTrue(false);
+//    }
 }
