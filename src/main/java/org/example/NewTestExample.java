d1e72263d64da64df5c5cc98ad1b4582c12b7d16
Aruna Abburi <aruna.abburi@yahoo.com>
2024-11-10 21:51:50 +0100

    6 tester created

--- src/main/java/org/example/NewTestExample.java
+++ src/main/java/org/example/NewTestExample.java
@@ -5,4 +5,18 @@ public class NewTestExample {
     public int add(int a, int b) {
         return a + b;
     }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double dividing(int a, int b) {
+
+        return (double) a/ (double) b;
+    }
+    public double squareRootOf(int a) {
+
+        return Math.sqrt(a);
+    }
 }
--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -2,22 +2,11 @@ import org.example.NewTestExample;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NewTest {
 
-    @Test
-    public void addvalue(){
-        Assert.assertTrue(true);
-    }
-    @Test
-    public void addvalue2(){
-        Assert.assertTrue(true);
-    }
     @Test
     @DisplayName("addition method success")
     public void addtwonumbes() {
@@ -25,13 +14,41 @@ public class NewTest {
         int value = cal.add(5, 5);
         assertEquals(10, value);
     }
+    @Test
+    @DisplayName("subtration method success")
+    public void subtractiontwonumbers(){
+        NewTestExample cal = new NewTestExample();
+        int value = cal.subtract(7,5);
+        assertEquals(2, value);
+    }
 
+    @Test
+    @DisplayName("multiplication method success")
+    public void multiplytwonumbers(){
+        NewTestExample cal = new NewTestExample();
+        int value = cal.multiply(4,3);
+        assertEquals( 12,value);
+    }
 
+    @Test
+    @DisplayName("Division method success")
+    public void dividtwonumbers(){
+        NewTestExample cal = new NewTestExample();
+        double value = cal.dividing(6,3);
+        assertEquals(2,value);
+    }
 
+    @Test
+    @DisplayName("Square method success")
+    public void squrerootofvalue(){
+        NewTestExample cal = new NewTestExample();
+        Double value = cal.squareRootOf(8);
+        assertNotEquals(2,value);
+    }
 
+   @Test
+   public void assertvaluecheck(){
+        Assert.assertTrue(true);
+  }
 
-//    @Test
-//    public void addvalue3(){
-//        Assert.assertTrue(false);
-//    }
 }
