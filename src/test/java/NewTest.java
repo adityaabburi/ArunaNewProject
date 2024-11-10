87c53481cab7e28302ad49e595d3c2c297a2e83c
Aruna Abburi <aruna.abburi@yahoo.com>
2024-11-10 19:35:30 +0100

    Initial commit

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class NewTest {
+
+    @Test
+    public void addvalue(){
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void addvalue2(){
+        Assert.assertTrue(true);
+    }
+}
